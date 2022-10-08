e13e518b30b6bb072636f346a7a4a1f13b049a39
nunegabriel <nune.gabriel.gn@gmail.com>
2022-10-08 17:04:57 +0000

    linters-fixed

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import titleIcon from './img/refresh.png';
 import addIcon from './img/add.png';
 import todoList from './modules/functions.js';
 
-const check = ()=> {
-    const inputChecks = document.querySelectorAll('.check');
-    inputChecks.forEach((check) => {
-      check.addEventListener('change', (event) => {
-        let todoList = [];
-        let arr =[];
-        todoList = JSON.parse(localStorage.getItem('todoList') || arr);
-        todoList.forEach((todo) => {
-          if ((event.target.id === `input${todo.index}`) && (todo.completed === false)) {
-            todo.completed = true;
-            event.target.checked = true;
-            localStorage.setItem('todoList', JSON.stringify(todoList));
-          } else if ((event.target.id === `input${todo.index}`) && (todo.completed === true)) {
-            todo.completed = false;
-            event.target.checked = false;
-            localStorage.setItem('todoList', JSON.stringify(todoList));
-          }
-        });
+const check = () => {
+  const inputChecks = document.querySelectorAll('.check');
+  inputChecks.forEach((check) => {
+    check.addEventListener('change', (event) => {
+      let todoList = [];
+      const arr = [];
+      todoList = JSON.parse(localStorage.getItem('todoList') || arr);
+      todoList.forEach((todo) => {
+        if ((event.target.id === `input${todo.index}`) && (todo.completed === false)) {
+          todo.completed = true;
+          event.target.checked = true;
+          localStorage.setItem('todoList', JSON.stringify(todoList));
+        } else if ((event.target.id === `input${todo.index}`) && (todo.completed === true)) {
+          todo.completed = false;
+          event.target.checked = false;
+          localStorage.setItem('todoList', JSON.stringify(todoList));
+        }
       });
-   });
-}
+    });
+  });
+};
 
 const doInput = document.getElementById('id-input');
 const titleSpan = document.getElementById('title-icon');
